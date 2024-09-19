a18d69accada5cd3e137b8c8e349478e9fb4e317
Kristian Gray <kris@ebi.ac.uk>
2024-09-19 16:43:21 +0100

    CREATE TRIGGER should never have 'public.' in front of trigger name

--- pgnc-public.sql
+++ pgnc-public.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a314f28294e7b699b96bcbce1e555a61c4bbfc60f1e7986797064d7a9833a4b
-size 88447
+oid sha256:0ab659318d62d9b4b24f7f960aeb2dc5888589570ee92581e4ef0e12b01eda8a
+size 87891
