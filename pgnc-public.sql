8c1d92514fd55415b7ed3248c35b6b0a50cff94f
Kristian Gray <kris@ebi.ac.uk>
2024-09-26 14:24:47 +0100

    clean up of boiler plate

--- pgnc-public.sql
+++ pgnc-public.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ab659318d62d9b4b24f7f960aeb2dc5888589570ee92581e4ef0e12b01eda8a
-size 87891
+oid sha256:c2a3d7b824e3ae17b77ab6397d92b5ec79ab71432d2e5bb881e4a749b79352cb
+size 87514
