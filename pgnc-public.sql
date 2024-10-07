1104bae805bccd88c94e0c50b3206777ab9c1a00
Kristian Gray <kris@ebi.ac.uk>
2024-10-07 11:27:10 +0100

    Updated changes from live

--- pgnc-public.sql
+++ pgnc-public.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2a3d7b824e3ae17b77ab6397d92b5ec79ab71432d2e5bb881e4a749b79352cb
-size 87514
+oid sha256:9b7997d2707c9ebb7c73bd96ce192e48d4aa2e71d8d840f2dba5e75407c73fe8
+size 97064
